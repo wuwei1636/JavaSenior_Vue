d5856276560f8bc031f980f568c0ce64c5664fbb
likunsong <1537628435@qq.com>
2023-06-14 15:16:01 +0800

    添加不让报错

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+//  不让
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/essential',
+        'eslint:recommended'
+    ],
+    parserOptions: {
+        parser: '@babel/eslint-parser'
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    },
+    overrides: [
+        //这里是添加的代码
+        {
+            files: ['src/**'],   // 匹配src中的所有vue文件
+            rules: {
+                'vue/multi-word-component-names':"off",
+            } //给上面匹配的文件指定规则
+        },
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)'
+            ],
+            env: {
+                jest: true
+            }
+        }
+    ]
+}
+
